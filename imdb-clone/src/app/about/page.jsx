6c77c26d06aa778dcbade5c76257964cdb28d4ad
Chris Monney <chris.monney@gmail.com>
2024-11-09 22:46:57 +0000

    created the navbar component

---
+++ imdb-clone/src/app/about/page.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry&apos;s standard dummy text
+        ever since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release
+        of Letraset sheets containing Lorem Ipsum passages, and more recently
+        with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum.
+      </p>
+      <p>
+        Aenean convallis fringilla orci, ut ultrices dui sollicitudin sed. Donec
+        maximus pharetra ipsum, ac mollis ex pulvinar quis. Nunc vehicula
+        pharetra ullamcorper. Praesent laoreet lacinia lacus ac maximus. Morbi
+        rhoncus elementum sapien, a ornare turpis. Mauris dapibus ac ligula in
+        bibendum. Class aptent taciti sociosqu ad litora torquent per conubia
+        nostra, per inceptos himenaeos. Nunc placerat nibh non nisi varius
+        lobortis. Pellentesque cursus tincidunt rutrum. Nulla a nunc luctus,
+        elementum tellus quis, rutrum mauris. Vivamus eget libero sed neque
+        ultricies consequat a in arcu. Phasellus id convallis turpis. In laoreet
+        elit eros, quis faucibus quam hendrerit ac. Nunc ac erat nec velit
+        accumsan ullamcorper.
+      </p>
+      <p>
+        Nulla vitae ante eget ante pulvinar dictum. Nunc vitae euismod augue.
+        Aliquam ut est in magna rhoncus dictum. Praesent molestie erat sit amet
+        dolor suscipit semper. Lorem ipsum dolor sit amet, consectetur
+        adipiscing elit. Ut efficitur fermentum lorem, sed pharetra nulla. Sed
+        dignissim in elit et dignissim. Aenean laoreet purus eget quam malesuada
+        dignissim. Nam pellentesque ligula nec libero aliquam, ut rhoncus nisi
+        placerat. Etiam fermentum id purus vel euismod. Donec faucibus volutpat
+        magna. Integer consequat interdum mauris ac lobortis. In hac habitasse
+        platea dictumst. Praesent dignissim urna nibh, sit amet commodo libero
+        tristique at. Nunc libero risus, posuere a sollicitudin nec,
+        sollicitudin eget leo.
+      </p>
+    </div>
+  );
+}
